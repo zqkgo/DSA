999f4365658b8e731a0f8585ed86285f7a583151
张乾坤 <zhangqiankun@qiyi.com>
2018-04-21 11:29:29 +0800

    fix merge sort

--- sort/merge.c
+++ sort/merge.c
@@ -8,37 +8,43 @@ int main() {
     int arr[] = {1, 198, 56, 77, 5678, 0, 1, 2, 10, 3, 555, 4444};
     int len = sizeof(arr) / sizeof(arr[0]);
     mergeSort(arr, 0, len - 1);
+    printArr(arr, len);
 }
 
 void mergeSort(int arr[], int left, int right) {
     if (left < right) {
         int mid = (left + right) / 2;
-        mergeSort(arr, left, mid - 1);
-        mergeSort(arr, mid, right);
+        // mergeSort(arr, left, mid - 1);
+        // mergeSort(arr, mid, right); // maybe infinite loop, like mergeSort(arr, 0, 1)
+        mergeSort(arr, left, mid);
+        mergeSort(arr, mid + 1, right);
         merge(arr, left, mid, right);
     }
 }
 
 /**
- *  
+ *                  arr       
+ * ----------------------------------------
+ * .... | | | | | | | | | | | | | | | |.... 
+ * ----------------------------------------
+ *     left         mid             right
  */
-void merge(int arr[], int low, int mid, int high) {
-    int leftArrLen = mid - low + 1;
-    int rightArrLen = high - mid;
+void merge(int arr[], int left, int mid, int right) {
+    int leftArrLen = mid - left + 1;
+    int rightArrLen = right - mid;
     int leftArr[leftArrLen];
     int rightArr[rightArrLen];
 
     for (int i = 0; i < leftArrLen; i++) {
-        leftArr[i] = arr[i];
+        leftArr[i] = arr[left + i]; 
     }
     for (int j = 0; j < rightArrLen; j++) {
-        rightArr[j] = arr[leftArrLen + j];
+        rightArr[j] = arr[mid + 1 + j];
     }
 
-    // int mergedArr[leftArrLen+rightArrLen]; // 归并数组
-    int mergedPos = low; // 指向归并数组
-    int leftPos = 0; // 指向左数组
-    int rightPos = 0; // 指向右数组
+    int mergedPos = left; // point to merged array
+    int leftPos = 0; // point to left array
+    int rightPos = 0; // point to right array
 
     while (leftPos < leftArrLen && rightPos < rightArrLen) {
         if (leftArr[leftPos] < rightArr[rightPos]) {
